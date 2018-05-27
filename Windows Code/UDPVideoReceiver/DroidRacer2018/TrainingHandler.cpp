aa33b62f346fd2a49f4b0da758d0d1e8a5020464
Matthew Lee <matthew.lee-mattner@griffithuni.edu.au>
2018-05-27 18:29:04 +1000

    Class for storing training images in a directory and refrencing it in a csv file along with the angle and throttle

---
+++ Windows Code/UDPVideoReceiver/DroidRacer2018/TrainingHandler.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+
+class TrainingHandler {
+private:
+	std::string directory;
+	std::string filename;
+	std::vector<cv::Mat> images;
+	std::vector<float> angles;
+	std::vector<float> throttles;
+
+public:
+	TrainingHandler(std::string directory, std::string filename);
+	bool addSample(cv::Mat image, float angle, float throttle);
+	bool saveSamples();
+};
+
+
+
+TrainingHandler::TrainingHandler(std::string directory, std::string filename) {
+	this->filename = filename;
+	this->directory = directory;
+}
+
+bool TrainingHandler::addSample(cv::Mat image, float angle, float throttle) {
+	this->images.push_back(image);
+	this->angles.push_back(angle);
+	this->throttles.push_back(throttle);
+	return 1;
+}
+
+bool TrainingHandler::saveSamples() {
+	std::ofstream outFile;
+	outFile.open(this->directory + this->filename);
+	outFile << "image, angle, throttle\n";
+	std::string imageName;
+	for (int i = 0; i < this->images.size(); i++) {
+		imageName = this->directory + "image_" + std::to_string(i) + ".png";
+		cv::imwrite(imageName, this->images.at(i));
+		outFile << imageName + "," + \
+			std::to_string(this->angles.at(i)) + "," + \
+			std::to_string(this->throttles.at(i)) + "\n";
+	}
+	outFile.close();
+	return 1;
+}
